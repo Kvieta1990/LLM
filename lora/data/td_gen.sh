8db8541125d1d70e594cea146b5f39ff6b200606
Yuanpeng Zhang <zyroc1990@gmail.com>
2025-11-03 09:37:54 -0500

    fix the training data gen output bug

--- lora/data/td_gen.sh
+++ lora/data/td_gen.sh
@@ -12,8 +12,8 @@ echo "Python location: $(which python)"
 echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
 
 # Run through the training, test and validation data generation.
-python gen_mt_train_data_ollama.py --input train.jsonl
-python gen_mt_train_data_ollama.py --input test.jsonl
-python gen_mt_train_data_ollama.py --input valid.jsonl
+python gen_mt_train_data_ollama.py --input train_tmp.jsonl --output train_mt.jsonl
+python gen_mt_train_data_ollama.py --input test_tmp.jsonl --output test_mt.jsonl
+python gen_mt_train_data_ollama.py --input valid_tmp.jsonl --output valid_mt.jsonl
 
 conda deactivate
